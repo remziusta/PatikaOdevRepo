ac55bd3c1add852caf8634add1731560a2c447af
Remzi USTA <37660677+remziusta@users.noreply.github.com>
2021-06-15 13:28:49 +0300

    Added UcgenAlan.java

---
+++ src/com/remziusta/UcgenAlan.java
@@ -0,0 +1,23 @@
+package com.remziusta;
+
+import java.util.Scanner;
+
+public class UcgenAlan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float karsiKenar, komsuKenar, hipotenusKenar, u, cevre;
+
+        System.out.print("Karşı Kenar Uzunluğu : "); karsiKenar = sc.nextFloat();
+        System.out.print("Komşu Kenar Uzunluğu : "); komsuKenar = sc.nextFloat();
+        System.out.print("Hipotenüs Kenar Uzunluğu : "); hipotenusKenar = sc.nextFloat();
+
+        u = (karsiKenar + komsuKenar + hipotenusKenar) / 2.0f;
+
+        System.out.println("Üçgenin çevresi : " + (2 * u));
+
+        double alan = (u * (u - karsiKenar)* (u - komsuKenar) * (u-hipotenusKenar));
+
+        System.out.println("Üçgenin alanı : " + Math.sqrt(alan));
+
+    }
+}
