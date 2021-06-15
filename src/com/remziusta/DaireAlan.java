6ad7e9028011bd15a8600fbd2a9840b49f301b7e
Remzi USTA <37660677+remziusta@users.noreply.github.com>
2021-06-15 13:43:49 +0300

    Added DaireAlan.java

---
+++ src/com/remziusta/DaireAlan.java
@@ -0,0 +1,21 @@
+package com.remziusta;
+
+import java.util.Scanner;
+
+public class DaireAlan {
+
+    private final static float _PI = 3.14f;
+
+    public static void main(String[] args) {
+        float radius, angle, area;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Yarıçapı giriniz :"); radius = sc.nextFloat();
+        System.out.print("Açıyı giriniz :"); angle = sc.nextFloat();
+
+        area = (_PI * (radius * radius) * angle)/360.0f;
+
+        System.out.println("Değerini girmiş olduğunuz daire diliminin alanı :" + area);
+
+    }
+}
