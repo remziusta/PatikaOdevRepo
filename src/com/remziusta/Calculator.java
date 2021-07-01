01bfd1f8b08941ab3febbe50f9e4cbb12d1021e7
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-07-01 16:38:27 +0300

    Added Basic Calculator.java

---
+++ src/com/remziusta/Calculator.java
@@ -0,0 +1,40 @@
+package com.remziusta;
+
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("İlk Sayınız : ");
+        int first = sc.nextInt();
+        System.out.print("İkinci Sayınız : ");
+        int second = sc.nextInt();
+
+        
+        System.out.println("Çarpma - > *");
+        System.out.println("Bölme - > /");
+        System.out.println("Toplama - > +");
+        System.out.println("Çıkarma - > -"); 
+        System.out.print("İşleminizi seçin :");
+        String y =  sc.next();
+
+        switch (y.charAt(0)) {
+            case '*':
+                System.out.println("Sonuç : " + first * second);
+                break;
+            case '/':
+                if(first > second ) System.out.println("Sonuç : " + first / second);
+                break;
+            case '+':
+                System.out.println("Sonuç : " + (first + second));
+                break;
+            case '-':
+                System.out.println("Sonuç : " + (first - second));
+                break;
+            default:
+                System.out.println("Geçerli bir işlem seçiniz...");
+                break;
+        }
+    }
+}
