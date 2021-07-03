2e8fa626f2201f70e12d85ce24c2ff17cac9871d
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-07-03 13:46:12 +0300

    Added ChinaZodiac.java

---
+++ src/com/remziusta/ChinaZodiac.java
@@ -0,0 +1,17 @@
+package com.remziusta;
+
+import java.util.Scanner;
+
+public class ChinaZodiac {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Doğum Yılınız : ");
+        int birthYear = sc.nextInt();
+        
+        String[] zodiacArray = {
+            "Maymun", "Horoz", "Köpek", "Domuz", "Fare", "Öküz", 
+            "Kaplan", "Tavşan", "Ejderha", "Yılan", "At","Koyun"};
+
+        System.out.println("Çin Zodyağı Burcunuz : " + zodiacArray[birthYear % 12]);
+    }
+}
