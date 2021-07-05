c667b8610bb2fcbc0a8718e0db661615eb2c9c39
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-07-05 14:53:19 +0300

    Added Combination.java

---
+++ src/com/remziusta/Combination.java
@@ -0,0 +1,24 @@
+package com.remziusta;
+
+import java.util.Scanner;
+
+public class Combination {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("İlk kümenin eleman sayısı : ");
+        int n = sc.nextInt();
+        System.out.print("İkinci kümenin eleman sayısı : ");
+        int r = sc.nextInt();
+        int combinationResult = factorialCalculator(n) / (factorialCalculator(r) * factorialCalculator(n - r));
+        System.out.println("İki kümenin kombinasyon sayısı : " + combinationResult);
+            
+    }
+
+    public static int factorialCalculator(int k){
+        int result = 1;
+        for (int i = 1; i <= k; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
